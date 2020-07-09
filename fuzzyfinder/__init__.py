b9eb57eb5b2c1a598243950e853f373c6a45fef9
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-07-09 11:41:51 +0100

    remove long line

--- fuzzyfinder/__init__.py
+++ fuzzyfinder/__init__.py
@@ -1,2 +1,1 @@
 print("hi")
-print("kjgashdflkjashdflkjahsdfhjsadlfhsldfafsdkalfhajh hldjhfljashdflkhsaldlsajdlsadlsahlsdlsahlsdjlsdfljlsdhlkjdlsdhlldjhlajlsjldjhlajdlskdlajdlskjdlsdldjhlhlajkjfdslslakh")
