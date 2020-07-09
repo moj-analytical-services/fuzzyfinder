466debc588bc2bef7c49ef9ee8632d186f9eda45
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-07-09 11:46:00 +0100

    testing black

--- fuzzyfinder/__init__.py
+++ fuzzyfinder/__init__.py
@@ -1,2 +1,4 @@
 print("hi")
-print("kjgashdflkjashdflkjahsdfhjsadlfhsldfafsdkalfhajh hldjhfljashdflkhsaldlsajdlsadlsahlsdlsahlsdjlsdfljlsdhlkjdlsdhlldjhlajlsjldjhlajdlskdlajdlskjdlsdldjhlhlajkjfdslslakh")
+
+def long_func(x, param_one=None, param_two=[], param_three={}, param_four=None, param_five="", param_six=123456):
+    print("This function has several params")
